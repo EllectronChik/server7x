7a77f7d42ae5e908adf908e1995307bb157169b3
ellectronchik <electronchik04@mail.ru>
2023-10-06 15:42:56 +0300

    New permission for admin or owner

--- main/permissions.py
+++ main/permissions.py
@@ -29,3 +29,10 @@ class isOwnerOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.user == request.user
+
+
+class isAdminOrOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.user == request.user or bool(request.user and request.user.is_staff)
