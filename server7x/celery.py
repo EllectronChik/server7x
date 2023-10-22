0ae261d1dc3f783aa50ce16cabd5991917d2de3d
EllectronChik <electronchik04@mail.ru>
2023-10-22 19:37:44 +0300

    dinamic task time

--- server7x/celery.py
+++ server7x/celery.py
@@ -1,6 +1,10 @@
 import os
+import datetime
 from celery import Celery
 from celery.schedules import crontab
+
+run_hour = datetime.datetime.utcnow().hour
+run_minute = datetime.datetime.utcnow().minute
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server7x.settings')
 
@@ -11,6 +15,9 @@ app.autodiscover_tasks()
 app.conf.beat_schedule = {
     'every': {
         'task': 'main.tasks.monthly_task',
-        'schedule': crontab(),
+        'schedule': crontab(hour=run_hour, minute=run_minute),
     }
 }
+app.conf.task_always_eager = False
+app.conf.task_reject_on_worker_lost = True
+app.conf.task_acks_late = True
