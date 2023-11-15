cc76f0a9fca890640d647b090e120daa3bede633
EllectronChik <electronchik04@mail.ru>
2023-11-15 19:49:36 +0300

    deleted CanPostOrIsAdmin

--- main/permissions.py
+++ main/permissions.py
@@ -48,12 +48,3 @@ class canEditMatchField(permissions.BasePermission):
                 return True
         return bool(request.user and request.user.is_staff)
     
-
-class CanPostOrIsAdmin(permissions.BasePermission):
-    def has_permission(self, request, view):
-        if request.method == 'POST':
-            return True
-        return bool(request.user and request.user.is_staff)
-    
-    def has_object_permission(self, request, view, obj):
-        return bool(request.user and request.user.is_staff)
