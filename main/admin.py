575afcb39566867ad439a8160c94ab7e7157202c
EllectronChik <electronchik04@mail.ru>
2023-11-20 22:26:42 +0300

    added widget for bool field

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import *
+from django import forms
 
 # Register your models here.
 admin.site.register(Team)
@@ -9,12 +10,37 @@ admin.site.register(ManagerContact)
 admin.site.register(TeamResource)
 admin.site.register(Stage)
 admin.site.register(Region)
-admin.site.register(Match)
 admin.site.register(Race)
 admin.site.register(AskForStaff)
 admin.site.register(League)
+admin.site.register(Season)
+admin.site.register(Tournament)
+admin.site.register(Schedule)
+admin.site.register(UserDevice)
+
 
 class LeagueFrameAdmin(admin.ModelAdmin):
     list_display = ('id', 'league', 'frame_max', 'region')
 
 admin.site.register(LeagueFrame, LeagueFrameAdmin)
+
+
+class CustomBooleanWidget(forms.widgets.Select):
+    def __init__(self, attrs=None):
+        choices = [(None, '---------'), (True, 'Player 1 wins'), (False, 'Player 2 wins')]
+        super().__init__(attrs, choices)
+
+    
+class MatchAdminForm(forms.ModelForm):
+    class Meta:
+        model = Match
+        fields = '__all__'
+        widgets = {
+            'winner': CustomBooleanWidget(),
+        }
+
+
+class MatchAdmin(admin.ModelAdmin):
+    form = MatchAdminForm
+
+admin.site.register(Match, MatchAdmin)
