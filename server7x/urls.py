97d503971809bbde1398c993f03505e192155098
EllectronChik <electronchik04@mail.ru>
2023-11-21 18:39:57 +0300

    added set_staff and get_current_tournaments

--- server7x/urls.py
+++ server7x/urls.py
@@ -6,7 +6,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 router = routers.DefaultRouter()
-router.register(r'ask_for_staff', views.AskForStaffViewSet, basename='ask_for_staff')
 router.register(r'teams', views.TeamsViewSet, basename='teams')
 router.register(r'players', views.PlayersViewSet, basename='players')
 router.register(r'managers', views.ManagersViewSet)
@@ -31,7 +30,10 @@ urlpatterns = [
     path('api/v1/get_players/<str:clan_tag>/', views.GetClanMembers.as_view()),
     path('api/v1/get_player_logo/<str:region>/<str:realm>/<str:character_id>/', views.GetMemberLogo.as_view()),
     path('api/v1/get_league_by_mmr/', views.get_league_by_mmr, name='get_league_by_mmr'),
+    path('api/v1/set_staff_true/', views.user_staff_status_true, name='set_staff_true'),
+    path('api/v1/set_staff_false/', views.user_staff_status_false, name='set_staff_false'),
     path('api/v1/manager/team/', views.get_team_and_related_data, name='get_team_and_related_data'),
+    path('get_current_tournaments', views.get_current_tournaments, name='get_current_tournaments'),
     path('api/v1/is_auth/', views.is_authenticated),
     path('api/v1/status/', views.is_manager_or_staff),
     path('api/v1/auth/', include('djoser.urls')),
