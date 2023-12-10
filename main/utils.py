8455f0ad32761100480c5c286f321d789db58436
EllectronChik <electronchik04@mail.ru>
2023-12-10 21:59:14 +0300

    added distribute_teams_to_groups + error handle

--- main/utils.py
+++ main/utils.py
@@ -1,7 +1,9 @@
 import configparser
 import datetime
+import random
 import requests
 from rest_framework.response import Response
+from main.models import GroupStage, LeagueFrame
 
 
 config = configparser.ConfigParser()
@@ -10,14 +12,28 @@ config.read('.ini')
 async def get_blizzard_league_data(region, league):
     token = config['BLIZZARD']['BLIZZARD_API_TOKEN']
     season = await get_season()
+    match region:
+        case 'eu':
+            region_multi = 0
+        case 'us':
+            region_multi = 1
+        case 'kr':
+            region_multi = 2
     api_url = f'https://{region}.api.blizzard.com/data/sc2/league/{season}/201/0/{league - 1}?locale=en_US&access_token={token}'
     response = requests.get(api_url)
     if response.status_code == 200:
         data = response.json()
-        
         for tier in data['tier']:
             if tier['id'] == 0:
-                return tier['max_rating']
+                if tier['max_rating']:
+                    return tier['max_rating']
+                else:
+                    last_max_id = region_multi * 6 + league - 1
+                    try:
+                        last_max = LeagueFrame.get(id=last_max_id).frame_max
+                    except:
+                        last_max = 0
+                return last_max + 500
 
     elif response.status_code == 401:
         get_new_access_token()
@@ -43,7 +59,6 @@ async def get_season():
     current_time = datetime.datetime.utcnow().isoformat()
 
     get_season = f'https://sc2pulse.nephest.com/sc2/api/season/state/{current_time}Z/DAY'
-    print(get_season)
 
     response = requests.get(get_season)
 
@@ -87,6 +102,8 @@ def get_blizzard_data(region, realm, character_id):
     token = config['BLIZZARD']['BLIZZARD_API_TOKEN']
     api_url = f'https://eu.api.blizzard.com/sc2/metadata/profile/{region}/{realm}/{character_id}?locale=en_US&access_token={token}'
     response = requests.get(api_url)
+    print(api_url)
+    print(response)
     if response.status_code == 200:
         return response
     elif response.status_code == 401:
@@ -94,3 +111,30 @@ def get_blizzard_data(region, realm, character_id):
         return get_blizzard_data(region, realm, character_id)
     else:
         return Response({"error": "Character not found"}, status=404)
+    
+
+def distribute_teams_to_groups(teams, num_groups):
+    try:
+        random.shuffle(teams)
+        num_groups = int(num_groups)
+    except:
+        return Response({"error": "Invalid number of groups"}, status=400)
+    try:
+        GroupStage.objects.filter(season=teams[0].season).delete()
+    except IndexError:
+        pass
+    teams_per_group = len(teams) // int(num_groups)
+    if teams_per_group == 0:
+        teams_per_group = 1
+    for group_mark in range(ord('A'), ord('A') + num_groups):
+        group_mark = chr(group_mark)
+        try:
+            group_stage, created = GroupStage.objects.get_or_create(
+                season = teams[0].season,
+                groupMark = group_mark
+            )
+            for i in range(teams_per_group):
+                team = teams.pop(0).team
+                group_stage.teams.add(team)
+        except IndexError:
+                break
