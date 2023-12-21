4e4f86a92b85cdbdf488282cde9833637dc834d6
EllectronChik <electronchik04@mail.ru>
2023-12-21 19:18:55 +0300

    new fields for Tournament

--- main/models.py
+++ main/models.py
@@ -55,6 +55,8 @@ class Tournament(models.Model):
                                  on_delete=models.PROTECT,
                                  related_name='team_two',)
     match_start_time = models.DateTimeField()
+    ask_for_other_time = models.DateTimeField(null=True, blank=True, default=None)
+    asked_team = models.ForeignKey('Team', on_delete=models.PROTECT, null=True, blank=True, default=None)
     season = models.ForeignKey('Season', on_delete=models.PROTECT)
     stage = models.IntegerField()
     group = models.ForeignKey('GroupStage', on_delete=models.CASCADE, null=True, blank=True, default=None)
