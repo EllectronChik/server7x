b57f2ca59b8fac8e3bb68072e969e499833c5b24
EllectronChik <electronchik04@mail.ru>
2024-01-08 18:37:53 +0300

    added ASGI

--- server7x/asgi.py
+++ server7x/asgi.py
@@ -10,7 +10,21 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from main.consumers import ScoreConsumer, MatchConsumer
+from django.urls import re_path, path
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server7x.settings')
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    'http': django_asgi_app,
+    'websocket': AuthMiddlewareStack(URLRouter([
+        path('ws/tournament_score/', ScoreConsumer.as_asgi()),
+        path('ws/match/', MatchConsumer.as_asgi()),
+    ])),
+})
+
