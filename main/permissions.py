33503d649d87e33c6b1663023fd200f8e2dad6ec
EllectronChik <electronchik04@mail.ru>
2024-02-21 15:47:12 +0300

    add comments, change CaseStyle and del unused permissions

--- main/permissions.py
+++ main/permissions.py
@@ -1,50 +1,74 @@
-from rest_framework import permissions
+from rest_framework import permissions    
 
-
-# class isOwnerOrAdminOrReadOnly(permissions.BasePermission):
-#     def has_object_permission(self, request, view, obj):
-#         if request.method in permissions.SAFE_METHODS:
-#             return True
-#         if obj.user == request.user:
-#             return True
-#         if bool(request.user and request.user.is_staff):
-#             return True
-#         return False
-    
-#     def has_permission(self, request, view):
-#         if request.method in permissions.SAFE_METHODS:
-#             return True
-#         return bool(request.user and request.user.is_staff)
-    
-
-class isAdminOrReadOnly(permissions.BasePermission):
+class IsAdminOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to allow only admin users to modify resources,
+    while allowing read-only access to other users.
+    """
     def has_permission(self, request, view):
+        """
+        Determine if the user has permission to access the view.
+
+        Args:
+            request (HttpRequest): The incoming HTTP request.
+            view (Any): The view being accessed.
+
+        Returns:
+            bool: True if the user has permission, False otherwise.
+        """
         if request.method in permissions.SAFE_METHODS:
+            # Allow read-only access for safe methods (GET, HEAD, OPTIONS).
             return True
+        # Allow modification only if the user is an admin.
         return bool(request.user and request.user.is_staff)
-    
 
-class isOwnerOrReadOnly(permissions.BasePermission):
+
+class IsAdminOrOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to allow admin users or object owners to modify resources,
+    while allowing read-only access to other users.
+    """
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return obj.user == request.user
+        """
+        Determine if the user has permission to modify the object.
 
+        Args:
+            request (HttpRequest): The incoming HTTP request.
+            view (Any): The view being accessed.
+            obj (Any): The object being accessed.
 
-class isAdminOrOwnerOrReadOnly(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
+        Returns:
+            bool: True if the user has permission, False otherwise.
+        """
         if request.method in permissions.SAFE_METHODS:
+            # Allow read-only access for safe methods (GET, HEAD, OPTIONS).
             return True
+        # Allow modification if the user is the owner of the object or is an admin.
         return obj.user == request.user or bool(request.user and request.user.is_staff)
 
 
-class canEditMatchField(permissions.BasePermission):
+class CanEditMatchField(permissions.BasePermission):
+    """
+    Custom permission to allow editing of match fields by players or admin users.
+    """
     def has_object_permission(self, request, view, obj):
+        """
+        Determine if the user has permission to edit the match field.
+
+        Args:
+            request (HttpRequest): The incoming HTTP request.
+            view (Any): The view being accessed.
+            obj (Any): The object being accessed.
+
+        Returns:
+            bool: True if the user has permission, False otherwise.
+        """
         if request.method in permissions.SAFE_METHODS:
+            # Allow read-only access for safe methods (GET, HEAD, OPTIONS).
             return True
         
+        # Allow modification if the user is one of the players or is an admin.
         if request.user and obj.player_one and obj.player_two:
             if (request.user == obj.player_one.user) or (request.user == obj.player_two.user):
                 return True
         return bool(request.user and request.user.is_staff)
-    
